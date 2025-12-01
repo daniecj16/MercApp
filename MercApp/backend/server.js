36c346a98c7bcf046cf32a383edfbc9b058d6af9
Daniel <dcruzj@est.ups.edu.ec>
2025-12-01 15:18:11 -0500

    Add new allowed origin to CORS configuration

--- MercApp/backend/server.js
+++ MercApp/backend/server.js
@@ -27,7 +27,8 @@ app.use(express.json());
 const allowedOrigins = [
   "http://localhost:5173",
   "https://merc-app-git-main-daniecj16s-projects.vercel.app",
-  "https://merc-m3cf41hdk-daniecj16s-projects.vercel.app"
+  "https://merc-m3cf41hdk-daniecj16s-projects.vercel.app",
+  "https://merc-app-eight.vercel.app/"
 ];
 
 
